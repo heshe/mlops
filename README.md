d342d7845b6851a5d340bf44a5fa3f952befc487
Thomas Dahl Heshe <tdh@veo.co>
2021-06-07 13:59:19 +0200

    updated repo with tsne plot

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # mlops
+### Train model:
+- Run src/models/main.py train
+
+### Evaluate model:
+- Run src/models/main.py evaluate
+
+### visualization:
+Make a t-sne plot of mnist:
+- Run src/visualization/visualize.py
---
+++ cookiecutter_project/src/models/predict_model.py
@@ -0,0 +1,46 @@
+import sys
+import argparse
+
+import torch
+
+from src.data.make_dataset import load_mnist
+from model import Classifier
+import matplotlib.pyplot as plt
+    
+if __name__ == '__main__':
+
+        parser = argparse.ArgumentParser()
+
+        parser.add_argument("--model_path", default="models/model29.pth")
+        parser.add_argument("--input_images", default="")
+        args = parser.parse_args()
+
+
+        # load model
+        model = Classifier()
+        state_dict = torch.load(args.model_path)
+        model.load_state_dict(state_dict)
+        
+        # Evaluation 
+        if args.input_images:
+            # Implement loading of image
+            print("Not supported")
+        else:
+            _, test_set = load_mnist()
+        testloader = torch.utils.data.DataLoader(test_set, batch_size=256, shuffle=True)
+        with torch.no_grad():
+                model.eval()
+                res = torch.zeros(0)
+                for images, labels in testloader:
+                    # Get the val loss
+                    log_ps = model(images)
+
+                    # Get the class probabilities
+                    ps = torch.exp(log_ps)
+                    _, top_class = ps.topk(1, dim=1)
+
+                    equals = top_class == labels.view(*top_class.shape)
+                    res = torch.cat((res, equals), dim=0)
+                accuracy = torch.mean(res.type(torch.FloatTensor))
+
+        print("Accuracy is: ", accuracy.item())
