da91373b0c6098e49e4363743e3e6faff5528abe
Thomas Dahl Heshe <tdh@veo.co>
2021-06-09 15:54:06 +0200

    refactoring

--- src/models/model.py
+++ src/models/model.py
@@ -22,7 +22,7 @@ class Classifier(nn.Module):
             raise ValueError("Expected input to a 4D tensor")
         if x.shape[1] != 1 or x.shape[2] != 28 or x.shape[3] != 28:
             raise ValueError(
-                f"Expected each sample to have shape [1, 28, 28], but {x.shape}"
+                f"Expected sample to have shape [1, 28, 28], but {x.shape}"
             )
 
         # Now with dropout
